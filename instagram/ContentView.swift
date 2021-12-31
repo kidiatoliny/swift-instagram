1c967f4cb45f372fba81ad5db83c775c20927963
kid <kidiatoliny@gmail.com>
2021-12-31 22:37:25 -0100

    Initial Commit

---
+++ instagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  instagram
+//
+//  Created by Kidiatoliny Gonçalves on 31/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ instagram/instagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramApp.swift
+//  instagram
+//
+//  Created by Kidiatoliny Gonçalves on 31/12/21.
+//
+
+import SwiftUI
+
+@main
+struct instagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
